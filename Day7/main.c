b5b294a745e9fc0bb9d5ce07b9ce10e2e51af6a3
kiranmaikarru-a11y <kiranmai_karru@srmap.edu.in>
2025-11-01 08:08:14 +0000

    main.c
    Added a simple banking system with deposit, withdrawal, and balance check functionalities.

--- Day7/main.c
+++ Day7/main.c
@@ -1,1 +1,43 @@
-
+4#include<stdio.h>
+int main(){
+    int ch;
+    float bal=10000,amt,bns;
+    do{
+        printf("\n1.Deposit\n2.Withdraw\n3.Check Balance\n4.Exit\nEnter choice: ");
+        scanf("%d",&ch);
+        if(ch==1){
+            printf("Enter amt to deposit: ");
+            scanf("%f",&amt);
+            if(amt>25000){
+                bns=amt*0.01;
+                amt+=bns;
+                printf("Bonus of ₹%.0f added!\n",bns);
+            }
+            bal+=amt;
+            bal-=5;
+            printf("Service charge ₹5 applied.\nUpdated Balance: ₹%.0f\n",bal);
+        }
+        else if(ch==2){
+            printf("Enter amt to withdraw: ");
+            scanf("%f",&amt);
+            if(amt>bal){
+                printf("Insufficient balance!\n");
+            }
+            else{
+                bal-=amt;
+                bal-=5;
+                printf("Service charge ₹5 applied.\nUpdated Balance: ₹%.0f\n",bal);
+            }
+        }
+        else if(ch==3){
+            printf("Current Balance: ₹%.0f\n",bal);
+        }
+        else if(ch==4){
+            printf("Thank you for banking with us!\n");
+        }
+        else{
+            printf("Invalid choice!\n");
+        }
+    }while(ch!=4);
+    return 0;
+}
